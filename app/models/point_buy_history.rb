799ade76a236c8b6fda00c6608efe3d391a5eb54
mei1161 <ichigo_happy@ezweb.ne.jp>
2020-06-09 09:45:02 +0900

    Create a model of point history

---
+++ app/models/point_buy_history.rb
@@ -0,0 +1,23 @@
+# == Schema Information
+#
+# Table name: point_histories
+#
+#  id         :bigint           not null, primary key
+#  date       :date
+#  integer    :point
+#  type       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  parent_id  :bigint
+#  user_id    :bigint
+#
+# Indexes
+#
+#  index_point_histories_on_user_id  (user_id)
+#
+class PointBuy < PointHistory
+  belongs_to :user
+  validates :point, presence: true
+  validates :date, presence: true
+  validates :parent_id, presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_08_124920) do
+ActiveRecord::Schema.define(version: 2020_06_08_135605) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,17 @@ ActiveRecord::Schema.define(version: 2020_06_08_124920) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "point_histories", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "type"
+    t.point "integer"
+    t.date "date"
+    t.bigint "parent_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_point_histories_on_user_id"
   end
 
   create_table "point_masters", force: :cascade do |t|
