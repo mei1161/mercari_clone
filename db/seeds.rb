9a0a6de530e71b582a07115b66630de300b68bb3
mei1161 <ichigo_happy@ezweb.ne.jp>
2020-06-10 15:01:28 +0900

    create seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
 User.create!(email: 'mei@gmail.com', name: 'mei', password: 'password', password_confirmation: 'password',confirmed_at: DateTime.now)
+PointMaster.create!(amount: 100, price: 120, started_at: DateTime.now, ended_at: '2025-12-31')
+PointMaster.create!(amount: 500, price: 520, started_at: DateTime.now, ended_at: '2025-12-31')
+PointMaster.create!(amount: 1000, price: 1020, started_at: DateTime.now, ended_at: '2025-12-31')
