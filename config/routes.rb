70371153d42c45eb855d491e027ff418f3184513
mei1161 <ichigo_happy@ezweb.ne.jp>
2020-06-11 15:03:35 +0900

    add routing

--- config/routes.rb
+++ config/routes.rb
@@ -20,5 +20,7 @@ Rails.application.routes.draw do
   scope :point do
     resource :purchase
   end
+
+  resources :items, only[:index, :show, :create, :destroy, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
