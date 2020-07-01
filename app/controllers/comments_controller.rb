3ba3430d0004556ff34973a81006f6004c67628f
mei1161 <ichigo_happy@ezweb.ne.jp>
2020-07-01 09:39:59 +0900

    Change how to redirect

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,15 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
   def create
-    @item = Item.find_by(id: params[:item_id])
     @comment = current_user.comments.build(comment_params)
     @comment.save
-    redirect_to item_path(@item)
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     @comment = current_user.comments.find_by(item_id: params[:id])
     @comment.destroy
-    redirect_to item_path(params[:id])
+    redirect_back(fallback_location: root_path)
   end
 
   private
