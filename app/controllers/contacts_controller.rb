566fbf5701e4d006b1ad4343f24ccaa019dc2ba8
mei1161 <ichigo_happy@ezweb.ne.jp>
2020-07-03 14:43:11 +0900

    Prepare a page for contacts

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,4 @@
+class ContactsController < ApplicationController
+  before_action :authenticate_user!
+  def show; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,7 @@ Rails.application.routes.draw do
     member do
       post :buy
     end
+    resource :contact, only: [:show]
     resources :comments, only: %i[create destroy]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
