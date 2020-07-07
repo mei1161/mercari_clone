4763a8b9c2e84ab606c27f1e7e71d699c91bcabd
mei1161 <ichigo_happy@ezweb.ne.jp>
2020-07-07 10:02:37 +0900

    add optional: true

--- app/models/item.rb
+++ app/models/item.rb
@@ -26,7 +26,7 @@ class Item < ApplicationRecord
   serialize :images
   ## validation
   belongs_to :user
-  belongs_to :address
+  belongs_to :address, optional: true
   belongs_to :category
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
