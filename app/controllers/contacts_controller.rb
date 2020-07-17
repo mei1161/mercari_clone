fdc7a9cc188a44259446cd7d5b57c70b37de097a
mei1161 <42018565+mei1161@users.noreply.github.com>
2020-07-17 12:39:23 +0900

    Add branch process in contact controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,20 @@
 class ContactsController < ApplicationController
   before_action :authenticate_user!
-  def show; end
+  def show
+    @item = Item.find(params[:item_id])
+    pp @item
+    pp current_user
+    if current_user.id != @item.user_id
+      if current_user.id != @item.buyer_id
+        redirect_to item_path(@item)
+        return
+      end
+    end
+
+    if current_user.id == @item.buyer_id
+      render 'buyer_contact'
+    elsif current_user == @item.user
+      render 'owner_contact'
+    end
+  end
 end
