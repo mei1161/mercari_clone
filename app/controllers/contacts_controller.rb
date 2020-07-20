7a37d657c9a207edcc59dee48987306832eeb80b
mei1161 <42018565+mei1161@users.noreply.github.com>
2020-07-20 23:13:33 +0900

    delete pp2

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,7 +2,6 @@ class ContactsController < ApplicationController
   before_action :authenticate_user!
   def show
     @item = Item.find(params[:item_id])
-    pp @item
     if (current_user.id != @item.user_id) && (current_user.id != @item.buyer_id)
       redirect_to item_path(@item)
       return
