0a577f9ca48b770fb297cebbaf2050963b4899c9
mei1161 <42018565+mei1161@users.noreply.github.com>
2020-07-21 11:33:06 +0900

    Separate view of shipping notifications

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -12,12 +12,18 @@ class ContactsController < ApplicationController
     elsif current_user == @item.buyer
       render 'buyer_contact'
     end
-    render 'owner_contact' if current_user == @item.user
+
+    if current_user == @item.user && @item.transaction_status == 'shipping'
+      render 'owner_contact'
+    elsif current_user == @item.user
+      render 'owner_wait_review'
+    end
   end
 
   def change_status
     @item = Item.find(params[:item_id])
     @item.transaction_status = :wait_sender_review
     @item.save
+    redirect_to item_contact_path(@item)
   end
 end
