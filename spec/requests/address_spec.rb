82ca251a68023f681af46855ff5bb4f83c09ab10
mei1161 <42018565+mei1161@users.noreply.github.com>
2020-07-29 10:05:39 +0900

    Test that you can't buy at someone else's address

--- spec/requests/address_spec.rb
+++ spec/requests/address_spec.rb
@@ -9,6 +9,7 @@ RSpec.describe 'addresses', type: :request do
   let(:item) { create(:item, user_id: user2.id, category_id: category.id) }
   let(:my_item) { create(:item, user_id: user.id, category_id: category.id) }
   let(:address) { create(:address, user_id: user.id) }
+  let(:user2_address) { create(:address, user_id: user2.id)}
   let(:address_params) { attributes_for(:address, user_id: user.id)}
   let(:invalid_address_params) { attributes_for(:invalid_address_params, user_id: user.id)}
   let(:point_master) { create(:point_master) }
@@ -61,6 +62,24 @@ RSpec.describe 'addresses', type: :request do
       post item_procedure_path(my_item), params: { address: {id: address.id} }
       buy_item = Item.find(my_item.id)
       expect(buy_item.buyer_id).to be_nil
+    end
+  end
+
+  describe 'POST procedures' do
+    it '他人の住所で購入することはできない' do
+      user.confirm
+      user2.confirm
+      sign_in user
+      category
+      item
+      user2_address
+      pp user2.addresses
+      post purchase_path, params:{ point_master_id: point_master.id } 
+      get item_path(item)
+      get item_procedure_path(item)
+      expect{
+        post item_procedure_path(item), params: { address: {id: user2_address.id} }
+      }.to raise_error(ActiveRecord::RecordNotFound)
     end
   end
 
